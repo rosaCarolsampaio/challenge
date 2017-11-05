8d4c131db35115cdf227f6176bf641a7aff9db13
rosaCarolsampaio <sampaioes@hotmail.com>
2017-11-05 20:40:46 +0000

    Update AtivarNovoUsuarioSteps.java

--- src/Testes/AtivarNovoUsuarioSteps.java
+++ src/Testes/AtivarNovoUsuarioSteps.java
@@ -2,24 +2,37 @@ package testes;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
+import static org.junit.Assert.*;
 
 public class AtivarNovoUsuarioSteps {
 
 	private WebDriver driver;
-	private String emailXpath = "//input[@name='login[email]']";
-	private String senhaXpath = "//input[@name='login[password]']";
-	private String senhaRepeteXpath = "//input[@name='login[repeat_password]']";
-	private String btnCasdastraXpath = "//button[@type='submit']";
+	private String emailXpath = ".//*[@id='userEmail']";
+	private String senhaXpath = ".//*[@id='userPass']";
+	private String senhaRepeteXpath = ".//*[@id='userPass-repeat']";
+	private String checkLiTermosXpath = ".//*[@id='mainForm']/div[4]/div/div/div/label";
+	private String btnCasdastraXpath = ".//*[@id='mainForm']/div[6]/div/button";
+
+	// Acesso ao Gmail
+	private String loginGmailID = "identifierId";
+	private String btnNextLoginID = "identifierNext";
+
+	private String senhaGmailXpath = ".//*[@id='password']/div[1]/div/div[1]/input";
+	private String btnNextSenhaID = "passwordNext";
 
 	@Dado("^Acessar a página registrar novo usuário$")
 	public void acessar_a_página_registrar_novo_usuário() throws Throwable {
 
-		String baseUrl = "https://www.otodom.pl/konto/korzysci/";
+		String baseUrl = "https://www.otodom.pl/konto/rejestracja/";
 		System.setProperty("webdriver.chrome.driver",
 				"/home/sampaio/workspace/selenium/chromedriver");
 		driver = new ChromeDriver();
@@ -40,80 +53,118 @@ public class AtivarNovoUsuarioSteps {
 
 		driver.findElement(By.xpath(senhaXpath)).clear();
 		driver.findElement(By.xpath(senhaXpath)).sendKeys(senha);
-
 	}
 
-	@Dado("^Confirmar a senha do usuário \"(.*?)\"$")
-	public void confirmar_a_senha_do_usuário(String senhaRepete)
-			throws Throwable {
+	@Dado("^Repetir a senha do usuário \"(.*?)\"$")
+	public void repetir_a_senha_do_usuário(String senhaRepete) throws Throwable {
 
 		driver.findElement(By.xpath(senhaRepeteXpath)).clear();
 		driver.findElement(By.xpath(senhaRepeteXpath)).sendKeys(senhaRepete);
 	}
 
-	@Dado("^Aceitar o envio de boletins \"(.*?)\" marcado$")
-	public void aceitar_o_envio_de_boletins_marcado(String arg1)
-			throws Throwable {
+	@Dado("^Aceitar a leitura do Termo$")
+	public void aceitar_a_leitura_do_Termo() throws Throwable {
 
+		WebElement checkLiTermo = driver.findElement(By
+				.xpath(checkLiTermosXpath));
+		if (!checkLiTermo.isSelected()) {
+
+			Actions actionsVendedor = new Actions(driver);
+			actionsVendedor.moveToElement(checkLiTermo).click().build()
+					.perform();
+		}
 	}
 
-	@Dado("^solicitar a realização do novo cadastro de usuário$")
+	@Dado("^Solicitar a realização do novo cadastro de usuário$")
 	public void solicitar_a_realização_do_novo_cadastro_de_usuário()
 			throws Throwable {
-
 		driver.findElement(By.xpath(btnCasdastraXpath)).click();
 	}
 
-	@Dado("^Sistema exibir a mensagem de cadastro com sucesso$")
-	public void sistema_exibir_a_mensagem_de_cadastro_com_sucesso()
+	@Dado("^Sistema exibe a mensagem de cadastro com sucesso$")
+	public void sistema_exibe_a_mensagem_de_cadastro_com_sucesso()
 			throws Throwable {
 
+		WebElement mensagem2 = new WebDriverWait(driver, 30)
+				.until(ExpectedConditions.presenceOfElementLocated(By
+						.xpath("html/body/section/div/div/div/div/h3")));
+		if (!mensagem2.isSelected()) {
+			Actions actionsMensagem = new Actions(driver);
+			actionsMensagem.moveToElement(mensagem2).click().build().perform();
+			String mensagemPreVisualizacao = mensagem2.getText();
+			assertTrue(mensagemPreVisualizacao
+					.contains("Teraz aktywuj swoje konto!"));
+		}
 	}
 
-	@Dado("^sistema exibir Botão de acessar seu email cadastrado para ativação$")
-	public void sistema_exibir_Botão_de_acessar_seu_email_cadastrado_para_ativação()
+	@Dado("^Sistema exibe Botão de acessar seu email cadastrado para ativação$")
+	public void sistema_exibe_Botão_de_acessar_seu_email_cadastrado_para_ativação()
 			throws Throwable {
 
+		driver.findElement(By.linkText("Sprawdź e-mail")).click();
 	}
 
-	@Quando("^Solicitar o acesso ao email cadastrado$")
-	public void solicitar_o_acesso_ao_email_cadastrado() throws Throwable {
+	@Quando("^Solicitar o acesso ao email cadastrado para ativação$")
+	public void solicitar_o_acesso_ao_email_cadastrado_para_ativação()
+			throws Throwable {
 
+		String urlRedirecionada = driver.getCurrentUrl();
+		assertTrue(urlRedirecionada.contains("mail.google.com"));
 	}
 
-	@Quando("^Sistema redireciona para a página do email$")
-	public void sistema_redireciona_para_a_página_do_email() throws Throwable {
+	@Quando("^Informar o email cadastrado \"(.*?)\"$")
+	public void informar_o_email_cadastrado(String emailGmail) throws Throwable {
 
+		driver.findElement(By.id(loginGmailID)).clear();
+		driver.findElement(By.id(loginGmailID)).sendKeys(emailGmail);
+		driver.findElement(By.id(btnNextLoginID)).click();
 	}
 
-	@Quando("^Solicitar acesso ao email$")
-	public void solicitar_acesso_ao_email() throws Throwable {
+	@Quando("^Informar a senha do email cadastrado \"(.*?)\"$")
+	public void informar_a_senha_do_email_cadastrado(String senhaGmail)
+			throws Throwable {
 
-	}
+		WebElement password = new WebDriverWait(driver, 10)
+				.until(ExpectedConditions.presenceOfElementLocated(By
+						.name("password")));
 
-	@Quando("^Informar o email cadastrado$")
-	public void informar_o_email_cadastrado() throws Throwable {
+		if (!password.isSelected()) {
+			Actions actionsPassword = new Actions(driver);
+			actionsPassword.moveToElement(password).click()
+					.sendKeys(senhaGmail).build().perform();
 
-	}
-
-	@Quando("^Informar a senha do email cadastrado$")
-	public void informar_a_senha_do_email_cadastrado() throws Throwable {
-
+			driver.findElement(
+					By.xpath(".//*[@id='passwordNext']/content/span")).click();
+		}
 	}
 
 	@Quando("^Acessar o email enviado para ativação$")
 	public void acessar_o_email_enviado_para_ativação() throws Throwable {
 
+		WebElement listaEmail = new WebDriverWait(driver, 30)
+				.until(ExpectedConditions.presenceOfElementLocated(By
+						.xpath("//span[contains(.,'Aktywuj konto')]")));
+
+		if (!listaEmail.isSelected()) {
+			Actions actionsPassword = new Actions(driver);
+			actionsPassword.moveToElement(listaEmail).click().build().perform();
+		}
 	}
 
 	@Quando("^Solicitar a ativação pelo link no email$")
 	public void solicitar_a_ativação_pelo_link_no_email() throws Throwable {
 
+		driver.findElement(By.xpath("//a[contains(.,'Aktywuj konto')]"))
+				.click();
 	}
 
 	@Então("^Sistema redireciona para página de ativação no sistema$")
 	public void sistema_redireciona_para_página_de_ativação_no_sistema()
 			throws Throwable {
+
+		String urlRedirecionada = driver.getCurrentUrl();
+		String urlExpect = "https://www.otodom.pl/";
+		assertEquals(urlExpect, urlRedirecionada);
 
 	}
 
@@ -121,12 +172,15 @@ public class AtivarNovoUsuarioSteps {
 	public void sistema_exibe_a_mensagem_de_ativação_de_usuário_com_sucesso(
 			String arg1) throws Throwable {
 
+		String mensagemAtivacao = driver.findElement(
+				By.xpath(" //h3[contains(.,'Gratulacje!')]")).getText();
+		assertTrue(mensagemAtivacao.contains("Gratulacje!"));
+		
+		//titlePage= Potwierdzenie
 	}
 
-	@Então("^Página  do teste é fechada$")
+	@Então("^Página do teste é fechada$")
 	public void página_do_teste_é_fechada() throws Throwable {
-
 		driver.quit();
 	}
-
 }
